dd576b55b67e0ce3bd8ba6e6f26eb051c9b927ef
Suresh <suresh@goquo.com>
2023-02-23 14:08:39 +0530

    Hosted & Direct Api integration headers added in checkout pages. and demo credenitals removed from the constantkeys

--- Utility/Helpers/ConstantKeys.cs
+++ Utility/Helpers/ConstantKeys.cs
@@ -7,56 +7,26 @@
 		public const string SandboxHostedPaymentEndpoint = "https://sandbox.pay.wowpay.io/hosted/payment";
 
 		public const string WowpaypublicKey = @"-----BEGIN PUBLIC KEY-----
-                                                MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAw48XhGaUIqpPHm3+tcZu
-                                                iWjUG+xxXOAOax7DaTcrajfUZuCCvHvV5G6pc2oqnuEiApi62qqLfMgiideAD/96
-                                                bXTXpNT1POm2sy53HmXo4zo9cUqBeoJeb+5tWrjJYYtJN7OdTioC9zG0aiZqBaji
-                                                VezBAQL29H0dx8x/xwllhd4OoLPkUJ3FNXmQfulZPAAY32fo9xjEf4/i5UZXVyss
-                                                n08elz8pz7dQgZorvyb29lNDMZ7NpkY+1zjKKx6igE2+pVGTRBiyh4ClBR+8r/rr
-                                                d2inwvzZ+GrqDVxKTLdbZ+XRL+SsVHplmJYT1qfEvocwCr+kqXH+iVVKlOyBPd3X
-                                                5QIDAQAB
+                                                XXX
                                                 -----END PUBLIC KEY-----";
 
 		public const string Wowpayprivatekey = @"-----BEGIN RSA PRIVATE KEY-----
-                                                MIIEowIBAAKCAQEAkTF7IgOvmqvjKnYBs1THTP6SP3pW43rDsdVoSYAJvirXnWxw
-                                                j8h/8p+9pZm5FHRxJhTco5q1czjyXoCiCJYzJyeN2RebHuD1w75wI/9mWCWYa+Gg
-                                                x/TmxiLIcL+XCZUXEsgLB0Pq4rWVbjpJqRcmHvxv/zFpyEyZU5Tnp4sCgX8a0OZ7
-                                                jpzYwWqiTaGdAHaKt5pjJwDsPFWChcCq6C5kWZITrg6/rE8n3yhp42N4G2RCWrCZ
-                                                JjV4Tget/teiizh02JwJk76Z9O6uZAE3z8mhjnw6TGguP4J7t9VmLkQX66E4QdPr
-                                                /vanXiTeX/IqD5zQUuq237mefa/h16SNKsEDSwIDAQABAoIBACDgQuDbU85igGAT
-                                                c2QfXLd25zefZIBCwoAIcgI+jzYzrjnTnZM5E5ulBwSxz6ItkZdNXv5eoBp0eRDj
-                                                8IygMfn6L1jKSxINXyjQ/MRx5sMr1B0NbGIKdpzDsH4zK6h8TUqv6CtfehaWhjz8
-                                                zsQOXU/j2ho/HpQ/n/I4g9HAnLMkdJQtFxibPRhJgUXSf4q7cZqMWXnqJrISxupJ
-                                                ++4ENZ38DNxaNxXy54w1NmMPlzc4uaNGGCx5xzongWW7GV5S7g4c9Vp1krcWGbeI
-                                                sETg/og3DMZKtPrpngYhhhKzOmhgUy4UFITbgl61V7H8C4/V05Q7Vof6+N9tlfFs
-                                                oYkiHR0CgYEA0iRmJdMAQIftzkpNrzN5DDJQF4mGWqzN9QEORnuMwNusw3n/odjk
-                                                9khl/yZ9FFjStmonCztC0F+xFnlppRJ4CofPUQ+Rv7R3zyRe6zcmVCxogHOLoHaI
-                                                vcN9867sxoRZs32M9NHnyMbVUEqiEVkwc72lZvTr11Icm/t8TpnAipcCgYEAsOC1
-                                                lbbhC0xx7KIubVFrcqtQEvmHfxtISusyaPqJrtb6dRaVfwipNm0Kmmpno9NoIy5u
-                                                SidT/iDJGKAieJYrIjaYRNblcB6NMtZTSIGsBAS/EMvR5YFxCRi9rLdv3Vo5i0xI
-                                                GAPNHMcwHcNojKSha1uXtVCIz13Evjr+pMi/J20CgYEAtrX7MqvxLiqBs7mbLdgd
-                                                SWN9i9mBPdm+AWZ3mQTXbuARqKysfXZuLUzpkh1O56ALJZvKctOF1lM9UAMkvef/
-                                                A3jt4tccrIgnG63N8cmzw8AQ5I6SOmSVpBkTrArNyBVPQObK5hqT41guJG+f1mGr
-                                                5RZhd9FANakpQt44SMcaR+UCgYB4Q1EzynSZShDr5BnNGl8ESJbZT/Bg7BvrVJ+h
-                                                4TdcOa9xjTl4dlsXNmzPMSnmqgdUZG143eSuFXyl0bueYfxR9uzxYWv/H8bBmhhm
-                                                ZEShNtjygqLkeW5x0PCmTtMwnfgiTmt2aJ89wfFlS0f62LUoWc586ZUb6ZWsAlDB
-                                                mfzewQKBgDta7dRaBolqzXir18y1IK43GFbSJAsWEwjD+rq62HP80J2N6CMDNuwC
-                                                HKQP64GOa7J+Zt0IsK4nmopT3ynNbkuRsSmGfBshPV1HoBs8wsdgTmGTQ+8GinmZ
-                                                8vRLR5O3PUv8cphS8PGWIc75EOyWeXSNNvoDghJ5YuPLEQVg831p
+                                               XXX
                                                 -----END RSA PRIVATE KEY-----";
 
-		public const string MerchantId = "612f70b7-f804-42f7-bbff-38788cfd3b16";
+		public const string MerchantId = "";
 
-		public const string MerchantCode = "ChanBrother_Staging";
+		public const string MerchantCode = "";
 
-		public const string UserName = "40c9f983-4a47-424d-ab46-2ec26e64b040";
+		public const string UserName = "";
 
-		public const string Password = "8KgN8AJju8NHOiN";
+		public const string Password = "";
 
 		public const string ProductCode = "ANY";
 
 		public const string Country = "ALL";
 
-		public const string InputVector = "PWhZs7rn18u4YbhI";
+		public const string InputVector = "";
 
 
 	}
